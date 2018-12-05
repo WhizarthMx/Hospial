39a0c2ab9c062f0d6a58a0b742506a472ad1f029
Juliooo <juliogrupo935@gmail.com>
2018-12-04 18:41:34 -0800

    new

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+    $host="ec2-23-21-201-12.compute-1.amazonaws.com";
+    $database="d9p2evje10ufd5";
+    $username="aaptteffoxntmr";
+    $password="a48046da330bc42f9590e2889ff7844a182b9ced077b3d2f1ec7b9f0bff5ebe2";
+
+
 return [
 
     /*
@@ -13,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,19 +62,17 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
+        'pgsql' => array(
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        ),
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
